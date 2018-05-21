7e83974d6f0bcfe239c619d38b1bd783cdaf63c5
Dmitry <kdp1999@yandex.ru>
2018-05-21 13:38:42 +0300

    add recovery page

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -3,15 +3,18 @@
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Адміністрування</h1>
         </div>
+        <user-recovery v-if="Number($store.state.user.admin_scope & 1) == 1"/>
         <user-validate v-if="Number($store.state.user.admin_scope & 2) == 2"/>
     </main>
 </template>
 
 <script>
     import UserValidate from './Permissions/UserValidate'
+    import UserRecovery from './Permissions/UserRecovery'
     export default {
         components: {
-            UserValidate
+            UserValidate,
+            UserRecovery
         }
     }
 </script>
---
+++ src/components/Admin/Permissions/UserRecovery.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-if="!data_fetching">
+    <h2>Відновлення</h2>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>ФІО</th>
+            <th>Роль</th>
+            <th>Група</th>
+            <th>Дії</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in users" :key="index">
+            <td>{{user.fio}}</td>
+            <td>{{user.role == 'teacher'? 'Викладач' : 'Студент'}}</td>
+            <td>{{user.role == 'teacher'? '-' : user.group.name}}</td>
+            <td>
+                <i @click="recovery(user.id, 'set')" class="fa fa-plus" aria-hidden="true"></i>
+                <i @click="recovery(user.id, 'delete')" class="fa fa-times" aria-hidden="true"></i>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data_fetching: true,
+        users: null
+      }
+    },
+    methods: {
+      load_data() {
+        this.data_fetching = true;
+        this.axios.get(`/users.get_recovery_users?v=1`).then(response => {
+          if(response.data.status == 200){
+            this.users = response.data.response
+            this.data_fetching = false;
+          }
+        })
+      },
+      recovery(user_id, command) {
+        if(command == 'set' || confirm("Відхилити?")){
+          this.axios.post(`/users.recovery`, {
+            v: '1',
+            command,
+            id: user_id
+          }).then(response => {
+            if(response.data.status == 200) {
+              this.load_data();
+            }
+          })
+        }
+      },
+    },
+    created() {
+      this.load_data();
+    }
+  }
+</script>
