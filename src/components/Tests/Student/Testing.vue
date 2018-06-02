b1358052559ca116fba2f1f022273316bf2ff55d
Dmitry <kdp1999@yandex.ru>
2018-06-02 15:42:28 +0300

    add student test statistic

--- src/components/Tests/Student/Testing.vue
+++ src/components/Tests/Student/Testing.vue
@@ -35,6 +35,39 @@
       <button @click="set_answer()" type="button" class="btn mt-3 mb-3 btn-success">Далі</button>
     </div>
 
+    <div v-if="next_step.status == 'complited'">
+      <h1 style="text-align: center;" class="mb-4">Тестування завершено</h1>
+      <div class="row">
+        <h4 class="col-2 d-flex justify-content-center">Варіант: {{next_step.stats.variant + 1}}</h4>
+        <h4 class="col-6 d-flex justify-content-center">Правильних відповідей: {{next_step.stats.true_answers}}</h4>
+        <h4 class="col-4 d-flex justify-content-center">Успішність: {{next_step.stats.success}}%</h4>
+      </div>
+
+      <div v-for="(question, q_index) in next_step.stats.questions" :key="q_index" class="question mt-1 pl-3">
+          <div class="form-group">
+              <div class="d-flex justify-content-between">
+                  <label class="text-primary font-weight-bold">Питання {{q_index + 1}}</label>
+              </div>
+              <div v-if="question.image_id != ''" class="d-flex justify-content-center qustion-image mb-3">
+                  <img :src="`${axios.defaults.baseURL}/photos.get?v=1&photo_id=${question.image_id}`" class="img-fluid img-thumbnail">
+              </div>
+              <textarea disabled v-model="question.name" class="form-control" rows="3"></textarea>
+          </div>
+
+          <div class="answers">
+              <div  class="form-group form-check">
+                  <div class="d-flex justify-content-between">
+                      <label  class="form-check-label font-weight-bold cursor-pointer" :class="question.right ? 'text-success' : 'text-danger'">
+                          Відповідь
+                      </label>
+                  </div>
+
+                  <textarea disabled v-model="question.text" class="form-control" rows="3"></textarea>
+              </div>
+          </div>
+      </div>
+    </div>
+
   </main>
   <main v-else role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="d-flex justify-content-center mt-5" >
